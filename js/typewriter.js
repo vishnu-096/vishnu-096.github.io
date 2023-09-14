0b5a39552d18a7f0dcd4dd4ccdb6f5a6094b6a0d
vishnu-096 <vishnu096@gmail.com>
2023-09-13 18:03:12 -0700

    a better portfolio. More changes coming in few days

---
+++ js/typewriter.js
@@ -0,0 +1,29 @@
+// JavaScript for typewriting effect
+function typeWriter(text, element, delay) {
+    let i = 0;
+    const speed = 50; // Adjust typing speed as needed
+    const targetElement = document.getElementById(element);
+
+    function type() {
+        if (i < text.length) {
+            targetElement.innerHTML += text.charAt(i);
+            i++;
+            setTimeout(type, speed);
+        } else {
+            // Reset the text and start typing animation again
+            setTimeout(() => {
+                targetElement.innerHTML = '';
+                i = 0;
+                type();
+            }, delay); // Delay before starting the animation again
+        }
+    }
+
+    // Start the typing animation
+    type();
+}
+
+// Call the typewriter function when the page loads
+window.addEventListener('load', () => {
+    typeWriter("HTML, CSS, JavaScript, Python, Robotics", 'skillsText', 1000); // Replace with your skills
+});
